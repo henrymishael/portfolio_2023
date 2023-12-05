056d692079fdab37dbf463f55894be1190ee215b
henrymishael <henrymishael.hm@gmail.com>
2023-12-05 23:43:26 +0100

    check

--- pages/works.js
+++ pages/works.js
@@ -6,21 +6,8 @@ import piggy from '@/public/assets/works/piggy.png'
 import port from '@/public/assets/works/portfolio.ico'
 import spotify from '@/public/assets/works/spotify.png'
 import tv from '@/public/assets/works/tv.png'
-import food1 from '@/public/assets/bg/food1.png'
-import food2 from '@/public/assets/bg/food2.png'
-import food3 from '@/public/assets/bg/food3.png'
-import food4 from '@/public/assets/bg/food4.png'
-import food5 from '@/public/assets/bg/food5.png'
-import food6 from '@/public/assets/bg/food6.png'
-import tmdb1 from '@/public/assets/portfolio-img2.png'
-import piggy1 from '@/public/assets/portfolio-img3.png'
-import piggy2 from '@/public/assets/bg/piggy2.png'
-import piggy3 from '@/public/assets/bg/piggy3.png'
-import piggy4 from '@/public/assets/bg/piggy4.png'
-import piggy5 from '@/public/assets/bg/piggy5.png'
-import piggy6 from '@/public/assets/bg/piggy6.png'
-import spot1 from '@/public/assets/portfolio-img4.png'
-import cover1 from '@/public/assets/portfolio-img5.png'
+
+
 
 
 
@@ -33,12 +20,7 @@ const projects = { previousWork : [
         slug: "food_app",
         image: food,
         link:'https://getfood.vercel.app/',
-        bg1: food1,
-        bg2: food2,
-        bg3: food3,
-        bg4: food4,
-        bg5: food5,
-        bg6: food6,
+    
     },
     {
         name:"Piggy Vest",
@@ -46,12 +28,7 @@ const projects = { previousWork : [
         slug: "piggy_vest",
         image: piggy,
         link:'https://my-projects-bay-nine.vercel.app',
-        bg1: piggy1,
-        bg2: piggy2,
-        bg3: piggy3,
-        bg4: piggy4,
-        bg5: piggy5,
-        bg6: piggy6
+       
         
     },
     {
@@ -60,9 +37,8 @@ const projects = { previousWork : [
         slug: "Movie",
         image: tv,
         link: 'https://moviebox-weld.vercel.app',
-        bg1: tmdb1,
-        bg2: '',
-        bg3: '',
+       
+        
         
     },
     {
@@ -71,9 +47,8 @@ const projects = { previousWork : [
         slug: "cover",
         image: cover,
         link:'https://image-gallery-vert-gamma.vercel.app',
-        bg1: cover1,
-        bg2: '',
-        bg3: '',
+        
+       
         
     },
     {
@@ -81,10 +56,8 @@ const projects = { previousWork : [
         id: 5,
         slug: "calc",
         image: calc,
-        link: 'hhttps://henrymishael.github.io/responsive_calculator',
-        bg1: '',
-        bg2: '',
-        bg3: '',
+        link: 'https://henrymishael.github.io/responsive_calculator',
+      
     },
     {
         name:"Spotify clone",
@@ -92,9 +65,8 @@ const projects = { previousWork : [
         slug: "spotify",
         image: spotify,
         link:'https://spotify-clone-henrymishael.vercel.app',
-        bg1: spot1,
-        bg2: '',
-        bg3: '',
+       
+        
     },
     {
         name:"portfolio v2",
@@ -102,9 +74,7 @@ const projects = { previousWork : [
         slug: "portfoliov2",
         image: port,
         link:'https://main--genuine-bublanina-013bba.netlify.app',
-        bg1: '',
-        bg2: '',
-        bg3: '',
+        
     }
 
 ]
